570cbb5bee404bb4cb84baea44f2eed2e1a57123
Victor Lin <bornstub@gmail.com>
2013-04-02 14:12:35 +0800

    Add contact us page

--- vic_pyramid/scaffolds/vic_pyramid/+package+/modules/front/__init__.py
+++ vic_pyramid/scaffolds/vic_pyramid/+package+/modules/front/__init__.py
@@ -1,3 +1,5 @@
 def includeme(config):
     config.add_route('front.home', '/')
     config.add_route('front.set_lang', '/set_lang/{lang}')
+    config.add_route('front.contact_us', '/contact_us')
+    config.add_route('front.contact_us_received', '/contact_us_received')
